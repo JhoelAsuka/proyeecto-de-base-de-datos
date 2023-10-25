7a0050ac034a4f6c07510a4f3a5465da06176b01
JhoelAsuka <143246834+JhoelAsuka@users.noreply.github.com>
2023-10-25 16:51:20 -0600

    Update menu2.php

--- prestafacil/src/vista/menu.php
+++ prestafacil/src/vista/menu.php
@@ -11,45 +11,47 @@
     <link rel="stylesheet" href="../controlador/bootstrap/css/style.css" />
 
 </head>
-<body>
-    <nav class="navbar navbar-expand-md bg-success-subtle">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="../vista/img/prestafacilcut.jpg" alt="">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          </button>
-          <div class="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active text-white text-bg-success rounded-4 border border-dark-subtle m-1" aria-current="page" href="#">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white text-bg-success rounded-4 border border-dark-subtle m-1" href="#">sobre nosotros</a>
-              </li>
-              <li class="nav-item dropdown ">
-                <a class="nav-link dropdown-toggle text-white text-bg-success rounded-4 border border-dark-subtle m-1" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  ¡Ve nuestro planes de pago!
-                </a>
-                <ul class="dropdown-menu ">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled text-white text-bg-success rounded-4 border border-dark-subtle m-1" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
+<body id="body2">
+  <header class="sticky">
+    <nav class="navbar navbar-expand-lg" >
+      <img src="/src/vista/img/prestafacilcut.jpg" class="logo" alt="">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse"  id="navbarNav">
+        <ul class="navbar-nav">
+          <li id="sex"> 
+            <a href="" id="menudrop"> INICIO</a>
+          </li>
+          <li id="sex"> 
+            <a href="" id="menudrop">Contacto</a>
+          </li>
+          <li id="sex">
+            <a href="" id="menudrop"> sobre nosotoros</a>
+          </li>
+          <li id="sex">
+            <a href="" id="menudrop">Quieres consultar un reporte?</a>
+          </li>
+          <li id="icon">
+            <i class="fa-solid fa-user-tie"></i>
+          </li>
+      </ul>
+      </div> 
       </nav>
-
+      </header>
+      <script type="text/javascript">
+        window.addEventListener("scroll", function(){
+          var header =document.querySelector("header");
+          header.classList.toggle("sticky", window.scrollY > 0);
+        })
+      </script>
+      <section class="banner">
+        <div class="content-banner">
+          <p>Necesitas un prestamo?</p>
+          <h2> ¡Nosotros podemos ayudarte!</h2>
+          <a href="">¡Ve nuestro planes¡</a>
+       </div>
+      </section>
       <div class="row">
         <div class="col">
           <div class="card">
@@ -60,27 +62,7 @@
       </div>
     </div>
         </div>
-        <div class="col-6"> <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="./img/menuu.jpg" width="100%" height="400px" class="d-block " alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="./img/moneda.jpg" width="100%" height="400px" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="./img/prestafacilcut.jpg" width="100%" height="400px" class="d-block w-100" alt="...">
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-          </div>
+        <div class="col-6"> 
         </div>
         <div class="col">
           <div class="card">
@@ -99,9 +81,63 @@
             <img src="../vista/img/menuu.jpg" >
         </div>
 
+        
 
 
 
-   <script src="/src/controlador/bootstrap/js/bootstrap.min.js"></script> 
+
+
+
+
+  
+
 </body>
+<footer class="footer">
+  <div class="conteiner container-footer">
+    <div class="menu-footer">
+      <div class="contacto_info">
+        <p class="tittle-footer">informacion de contacto</p>
+        <ul>
+          <li>Dirección:Av. Río San Miguel Núm. 12 Col. Tupuchu </li>
+          <li>Telefono:961 666 3318</li>
+          <li>Gmail:hellowo1@gmail.com</li>
+        </ul>
+        <div class="social-icons">
+          <span class="facebook"><i class="fa-brands fa-facebook-f"></i></span>
+          <span class="x"><i class="fa-solid fa-x"></i></span>
+          <span class="instagram"><i class="fa-brands fa-instagram"></i></span>
+        </div>
+      </div>
+      <div class="information">
+        <p class="tittle-footer">Información</p>
+        <ul>
+          <li><a href="#">Acerca de nosotros</a></li>
+          <li><a href="#">Información delivery  </a></li>
+          <li><a href="#">Políticas de Privacidad</a></li>
+          <li><a href="#">Terminos Y condiciones</a></li>
+          <li><a href="#">Contactános</a></li>
+        </ul>
+      </div>
+      <div class="my-account">
+        <p class="tittle-footer">Mi cuenta</p>
+        <ul>
+          <li><a href="#">Mi cuenta</a></li>
+          <li><a href="#">Historial</a></li>
+          <li><a href="#">Lista de deseps</a></li>
+          <li><a href="#">Boletin</a></li>
+        </ul>
+      </div>
+
+      </Div>
+    </div>
+    <div class="copyright">
+      <p>Desarrollado por System_eva &copy;
+
+      </p>
+    </div>
+
+  </div>
+<script src="/src/controlador/bootstrap/js/bootstrap.min.js"></script> 
+<script src="https://kit.fontawesome.com/8b617d5d0c.js" crossorigin="anonymous"></script>
+</footer>
 </html>
